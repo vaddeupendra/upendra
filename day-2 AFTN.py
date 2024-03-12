fe34fe734b1c07f93f1ec9944083c9144f4e4e97
vaddeupendra <162984926+vaddeupendra@users.noreply.github.com>
2024-03-12 16:17:44 +0530

    Add files via upload

---
+++ day-2 AFTN.py
@@ -0,0 +1,279 @@
+# ! // ---> floor devision
+# a= 765433
+# b=19
+# print(a//b) # -> the output wii be inn integer & the output will be
+# based on floor value
+
+#!  ** ----> used for power of a number
+# print(2**4) #--->16
+
+# ! assignment ---> +-=,-=,/=,*=,//=,**=,&=,|=,%=
+# a=8
+#a+=2
+232 # print(a)
+233
+234 #! comparison --> ==, >, <, =, <=, >=
+235 # a = 8
+236 # b = 7
+237 # print(a>b) # True
+238
+239 # a = 9
+240 # b = 5
+241 # print(a==b)
+
+# ! bitwise operator ---> &,|,^,~,<<,>>
+ a=7
+ b=4
+ print(a&b)
+
+
+ # 2^4 2^3 2^2 2^1 2^0
+ # 8   4   2    1
+
+ # 0   1   1    1   #---->7
+ # 0   1   0    0   #----->4&
+ #---------------------
+ # 0    1     0    0
+
+
+ # 256 128 64 32 16 8 4 2 1
+ # 0 262 # XOR
+263 # A B A^B
+264 #00
+265 #01 1
+266 # 1 0 1
+267 # 1 1 0
+268
+269 #0 0 1 0 1 0 #--> 10
+270 #00011
+271 #
+272
+273
+274 # 204 203 202 201 200
+275 # 8248 # AND
+249 # A B
+A&B
+0
+1
+250 #00
+251 #0 1
+252
+#
+10
+253 # 1 1
+254
+255
+# OR
+256 # A B
+AB
+0
+1
+1
+1
+257 #00
+258 #0 1
+259246 # print(alb)
+247
+248 # AND
+249
+# A B
+250 #00
+A&B
+kim.ny
+251
+#01
+0
+T
+253
+# 1 1
+1
+AB
+254
+255
+# OR
+256 # A B
+257
+#00
+0
+258 #0 1 1
+259 # 1
+0
+260 # 1 1
+1
+1
+#10
+260 # 1 1
+4
+2
+10 0 0 0 0 0 0 1 0 0 0
+
+# 32 16 8 4 2 1
+# 0  0  1  01 0 #---
+303 # 256 128 64 32 16 8 4 21
+304
+305 #0
+306
+# 107
+307
+308 # <<, >>
+309 # print(5>>1)
+310 # 16>4
+311
+0
+0
+0
+1 0
+1
+3<
+0 1000--> 40
+
+312 # Logical --> used to compare the expressions
+313 # and, or, not
+314 a = 6
+315 # print(a>3 and a<10)
+316 # print(a>7 or a < 30'
+317 # print(not(a>8 and a < 10 ) )
+318
+
+
+
+
+
+
+
+319 #! Identity operator
+320 #? It is used to compare the memory location that the values
+321 # are stored
+322
+323 # is, is not
+324 a = 8
+325 b = 8
+326 # print(a is b)
+327 # print(a==b)
+328
+329 #a= [1, 2, 3]
+330 # b = [1, 2, 3]
+331 # print(a is not b)
+
+
+# ! membership operator
+# in, not in
+
+# 11= [7,8,9,0,6,5]
+# num = 55
+# print(num in 11)
+# print ( num not 11)
+
+#num = 678
+# print (8 in num) # error
+
+
+47 # Eg:1
+48 # --> C syntax
+49 # if (condition){
+50
+#
+statement;
+51 #
+statement;
+52
+#
+statement;
+53 # }
+54
+Python syntax
+55 # if condoition:
+56
+#
+statement
+57
+#
+statement
+-58
+#
+statement
+59
+statement
+#
+
+361 # eg:1
+362 #a6
+63 # if a:
+$64 # print("hello")
+$65
+66 # Eg:2
+$67 # a = 6
+68 # if a>3:
+69 # print("hey")
+$70 # else:
+$71 # print("no")
+
+
+379 # eg:4
+380
+# a = 0
+381 # a = None
+382 # a=False
+383 # a=""
+384 # if a:
+385 # I print("yes")
+386 # else:
+387 #
+print("no")
+
+
+# a number is even or odd
+ n1=5
+ n2= 6
+ n1 is odd number
+ n2 is even number
+ # printf("enter even number")
+
+ 
+
+383 # a=""
+384 # if a:
+385 # print("yes")
+386 # else:
+387 #
+print("no")
+388
+389 # a number is even or odd
+390 n = int(input("enter the number: "))
+391 if n %2==0:
+392
+print(n, "is even")
+393
+else:
+394
+print(n, "is odd")
+
+# name ,age, nationality
+# upendra,20 above, indian
+name= input("what is your name?")
+age= input("how old are you?")
+nationalty= input("what is your nationality?")
+print("hello,", name +"!")
+print("you are",age, "years old, and you are from",nationality +","
+
+
+398 # sum:2
+399 # name, age, nationality
+400 # 18 above, Indian
+401
+402
+    name = input("Enter the name: ")
+403 age = int(input("enter the age: "))
+404 nationality=input("enter the nation: ")
+405
+    if age>=18 and nationality=="Indian":
+408     print("eligible for vote")
+    else:
+I
+409     print("not eligible")
+
+
+
+
+ 
+ 
